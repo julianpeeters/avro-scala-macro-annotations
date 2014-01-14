eafef6a7b56dc6c024490bf29d957b8dc23a69d2
julianpeeters <julianpeeters@gmail.com>
2014-01-13 21:37:17 -0800

    hmmmm dead end?

--- macros/src/main/scala/Macros.scala
+++ macros/src/main/scala/Macros.scala
@@ -6,21 +6,29 @@ import scala.annotation.StaticAnnotation
 
 import scala.io._
 
-case class Val(fieldName: String, fieldType: String)
+//case class Val(fieldName: String, fieldType: String)
 
 object valProviderMacro {
+
+
+
+
   def impl(c: Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
     import c.universe._
     import Flag._
 
     val fieldData = List(("x", "String"), ("myRec", "MyRec"))
     def asParam(fieldTypeName: Name) = {
-      fieldTypeName match {
-        case "String" => ""
-        case x  =>  x + """("hello macro")"""
+println(fieldTypeName)
+val h = "myRec"
+val g = q""""hello macro""""
+val i = newTypeName("MyRec")
+      fieldTypeName.toString match {
+        case "String" =>   {println("asPAram " ); q""" $h """}
+        case x  =>{   println("asPAram x " ); q"""$i($g)"""}//"MyRec(hello macro)" // x+ """(hello macro)"""
       }
     }
-   
+
     val result = {
       annottees.map(_.tree).toList match {
 
@@ -28,11 +36,26 @@ object valProviderMacro {
           val CASEACCESSOR = (1 << 24).toLong.asInstanceOf[FlagSet]
           val PARAMACCESSOR = (1 << 29).toLong.asInstanceOf[FlagSet]
           val valProviderMods = Modifiers(CASEACCESSOR | PARAMACCESSOR | DEFAULTPARAM)
-          val fieldTermName = newTermName(fieldData(0)._1)
-          val fieldTypeName = newTypeName(fieldData(0)._2)
+if (name.toString == "MyRec") { println("MyRec")
+          val fieldTermName = newTermName(fieldData(1)._1)
+          val fieldTypeName = newTypeName(fieldData(1)._2)
+println(fieldTypeName)
           val param = asParam(fieldTypeName)
           val valProviderVal = q"""$valProviderMods val $fieldTermName: $fieldTypeName = $param"""
           q"$mods class $name[..$tparams](..$first, $valProviderVal)(...$rest) extends ..$parents { $self => ..$body }"
+}
+else { println("not my rec")
+          val fieldTermName = newTermName(fieldData(0)._1)
+          val fieldTypeName = newTypeName(fieldData(0)._2)
+
+println(fieldTypeName)
+          val param = asParam(fieldTypeName)
+          val valProviderVal = q"""$valProviderMods val $fieldTermName: $fieldTypeName = $param"""
+          q"$mods class $name[..$tparams](..$first, $valProviderVal)(...$rest) extends ..$parents { $self => ..$body }"
+}
+
+
+
 
       }
     }
