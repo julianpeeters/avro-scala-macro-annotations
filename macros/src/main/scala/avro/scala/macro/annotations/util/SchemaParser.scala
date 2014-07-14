9ba413284173bc6a3bfa2710956b2a20e9ecd859
i-am-the-slime <mark.eibes@gmail.com>
2014-07-14 15:36:44 +0200

    Added option to get schema from .avsc files.

--- macros/src/main/scala/avro/scala/macro/annotations/util/SchemaParser.scala
+++ macros/src/main/scala/avro/scala/macro/annotations/util/SchemaParser.scala
@@ -4,15 +4,22 @@ package util
 import org.apache.avro.file.DataFileReader
 import org.apache.avro.generic.{GenericDatumReader, GenericRecord}
 import org.apache.avro.Schema
+import org.apache.avro.Schema.Parser
 import org.apache.avro.Schema.Type._
 
 import scala.collection.JavaConverters._
 
-object SchemaParser { 
+object SchemaParser {
   def getSchema(infile: java.io.File) = {
-    val gdr = new GenericDatumReader[GenericRecord]
-    val dfr = new DataFileReader(infile, gdr)
-    val schema = dfr.getSchema
+    val schema = infile.getName.split("\\.").last match {
+      case "avro" =>
+        val gdr = new GenericDatumReader[GenericRecord]
+        val dfr = new DataFileReader(infile, gdr)
+        dfr.getSchema
+      case "avsc" =>
+        new Parser().parse(infile)
+      case _ => throw new Exception("Invalid file ending. Must be .avsc for plain text json files and .avro for binary files.")
+    }
     schema.getType match {
       case UNION  => schema.getTypes.asScala.foreach(s => ClassFieldStore.storeClassFields(s))
       case RECORD => ClassFieldStore.storeClassFields(schema)
