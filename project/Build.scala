d371f0dca95ac62a2976e77efa3c3493ac3bc56b
julianpeeters <julianpeeters@gmail.com>
2015-02-24 03:59:40 -0800

    bump version to snapshot to test serialization without SCHEMA$ field

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import Keys._
 object BuildSettings {
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "com.julianpeeters",
-    version := "0.3",
+    version := "0.3-SNAPSHOT",
     scalacOptions ++= Seq(),
     scalaVersion := "2.10.4",
     libraryDependencies += "org.apache.avro" % "avro" % "1.7.6",
