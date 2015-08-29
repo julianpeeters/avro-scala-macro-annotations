929abad04b14fabc524e2cd4b8dd04d75fd99432
Julian Peeters <julianpeeters@gmail.com>
2015-08-28 23:18:59 -0700

    swapped pritln debug for info level llogging, bumped to 0.10.0-SNAPSHOT

--- macros/src/main/scala/avro/scala/macro/annotations/AvroTypeProviderMacro.scala
+++ macros/src/main/scala/avro/scala/macro/annotations/AvroTypeProviderMacro.scala
@@ -9,26 +9,30 @@ import scala.annotation.StaticAnnotation
 import collection.JavaConversions._
 import java.io.File
 
-object AvroTypeProviderMacro {
+import com.typesafe.scalalogging._
+
+object AvroTypeProviderMacro extends LazyLogging {
 
   def impl(c: Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
     import c.universe._
-    import Flag._ 
+    import Flag._
 
     //get the namespace from the context and passing it around instead of using schema.getNamespace
     //in order to read schemas that omit namespace (e.g. nested schemas or python's avrostorage default)
     val namespace = NamespaceProbe.getNamespace(c)
 
-    val result = { 
+    val result = {
       annottees.map(_.tree).toList match {
         case q"$mods class $name[..$tparams](..$first)(...$rest) extends ..$parents { $self => ..$body }" :: Nil => {
-         
+
           //currently, having a `@AvroRecord` the only thing that will trigger the writing of vars instead of vals
           val isImmutable: Boolean = {
             !mods.annotations.exists(mod => mod.toString == "new AvroRecord()" | mod.toString =="new AvroRecord(null)")
           }
-          
-          println(s"Current path: ${new File(".").getAbsolutePath}")
+
+          //helpful for IDE users who may not be able to easily see where there files live
+          logger.info(s"Current path: ${new File(".").getAbsolutePath}")
+
           val avroFilePath = FilePathProbe.getPath(c)
           val infile = new File(avroFilePath)
           val schema = FileParser.getSchema(infile)
